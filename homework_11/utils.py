2a5aa295349d96d5d93f38119f080fc692ee425d
r0mze <iren.mayorova@gmail.com>
2022-04-13 14:00:02 +0300

    initial commit homework 11

---
+++ homework_11/utils.py
@@ -0,0 +1,34 @@
+import json
+
+__data = []
+
+
+def load_candidates_from_json(path):
+    global __data
+    with open(path, 'r', encoding='utf-8') as file:
+        __data=json.load(file)
+    return __data
+
+def get_cadidates(candidate_id):
+    for candidate in __data:
+        if candidate['id'] == candidate_id:
+            return {
+                'name': candidate['name'],
+                'position': candidate['position'],
+                'picture': candidate['picture'],
+                'skills': candidate['skills']
+            }
+    return {'not_found': 'Ушёл на обед'}
+
+def get_candidates_by_name(candidate_name):
+    return [candidate for candidate in __data if candidate_name.lower() in candidate['name'].lower()]
+
+
+def get_candidates_by_skill(skill_name):
+    candidates = []
+    for candidate in __data:
+        skills = candidate['skills'].lower().split(', ')
+        if skill_name in skills:
+            candidates.append(candidate)
+    return candidates
+
